c42c7fe1a12782ba122f6dc0c848778e85024244
2266cano1113 <91908782+2266cano1113@users.noreply.github.com>
2022-11-01 20:10:19 -0600

    fondo 105 porciento

--- pages/index.jsx
+++ pages/index.jsx
@@ -291,10 +291,9 @@ export default function Home(AOS) {
               <h1>Diana
                 <span className='animationPink'>
                   {/* {rosaCentral} */}
-                  <svg width="26" height="26" fill="currentColor" class="bi bi-hearts" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M4.931.481c1.627-1.671 5.692 1.254 0 5.015-5.692-3.76-1.626-6.686 0-5.015Zm6.84 1.794c1.084-1.114 3.795.836 0 3.343-3.795-2.507-1.084-4.457 0-3.343ZM7.84 7.642c2.71-2.786 9.486 2.09 0 8.358-9.487-6.268-2.71-11.144 0-8.358Z" />
-                  </svg>
-                </span>{invitado.novio ? ('Ezequiel') : ('Cano')}</h1>
+                  &
+                </span>
+                {invitado.novio ? ('Ezequiel') : ('Cano')}</h1>
               {playM ? (
                 <audio src="/saborAmiMini.mp3" autoPlay loop> </audio>
               ) : (
